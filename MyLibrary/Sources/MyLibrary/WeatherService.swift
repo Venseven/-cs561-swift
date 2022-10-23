45b73c0877c820c3379e28e1cc46c98981103b91
Venkatesh S <47088590+Venseven@users.noreply.github.com>
2022-10-22 22:49:56 -0700

    Update WeatherService.swift

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -11,7 +11,7 @@ enum BaseUrl :String {
 class WeatherServiceImpl: WeatherService {
     // case switch between realAPI and MockServer
     // let url = "\(BaseUrl.moclserver)?lat=44.56&lon=123.26&appid=d8310c9fbcfaee650d22017af4646e00" 
-    let url = "https://api.openweathermap.org/data/2.5/weather?lat=44.56&lon=123.26&appid=d8310c9fbcfaee650d22017af4646e00"
+    let url =  "http://127.0.0.1:8000/data/2.5/weather?lat=44.56&lon=123.26&appid=d8310c9fbcfaee650d22017af4646e00"
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
             AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
